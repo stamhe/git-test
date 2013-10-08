ea7517f2c493eb8536e52db7588c702b576c1f9e
Stam He <stamhe@163.com>
2013-10-08 21:27:20 +0800

    20131008_v2

--- t.c
+++ t.c
@@ -6,6 +6,7 @@ int main(){
     printf("I'm from stam0\n");
     printf("I'm from stamhe\n");
     printf("20131008212300\n");
+    printf("20131008212700\n");
     printf("last one\n");
     return 0;
 }
